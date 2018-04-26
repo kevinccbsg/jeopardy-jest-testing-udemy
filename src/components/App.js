225661916f54b5507cb49fcc50cbe8a8fa4f4ca0
kevinccbsg <kevinccbsg@gmail.com>
2018-04-26 00:02:27 -0400

    Add redux to the application

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  async componentDidMount() {
+    const response = await fetch('http://jservice.io/api/categories?count=20')
+    const body = await response.json();
+    console.log(body);
+  }
+
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import App from './components/App';
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger),
+);
 
 render((
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 ), document.getElementById('root'));
