56a995923bdd79deece83e7fc192372c6c99d842
kevinccbsg <kevinccbsg@gmail.com>
2018-04-26 00:22:49 -0400

    React router and category component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './components/App';
+import Category from './components/Category';
 import rootReducer from './reducers';
 
 const store = createStore(
@@ -13,6 +15,11 @@ const store = createStore(
 
 render((
   <Provider store={store}>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/category" component={Category} />
+      </Switch>
+    </Router>
   </Provider>
 ), document.getElementById('root'));
