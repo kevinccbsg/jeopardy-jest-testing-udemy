a93a381035621e739b677a90f47014d585d31662
kevinccbsg <kevinccbsg@gmail.com>
2018-04-26 00:13:58 -0400

    Connected redux in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { setCategories } from '../actions';
 
 class App extends Component {
 
   async componentDidMount() {
-    const response = await fetch('http://jservice.io/api/categories?count=20')
-    const body = await response.json();
-    console.log(body);
+    const { categories } = this.props;
+    if (categories.length === 0) {
+      const response = await fetch('http://jservice.io/api/categories?count=20')
+      const body = await response.json();
+      this.props.setCategories(body);
+    }
   }
 
   render() {
@@ -17,4 +23,15 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => (
+  {
+    categories: state.categories,
+  }
+);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    setCategories,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
