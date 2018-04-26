af1dd5e4ff5e1ab42f618867edea93dee6d03844
kevinccbsg <kevinccbsg@gmail.com>
2018-04-26 01:10:12 -0400

    Clue integration with animation for answers

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Clue from './Clue';
 
 class Category extends Component {
   constructor() {
@@ -25,9 +26,7 @@ class Category extends Component {
         <Link to="/"><h4>Home</h4></Link>
         <h2>{category.title}</h2>
         {clues.map(clue => (
-          <div key={clue.id}>
-            {clue.question}
-          </div>
+          <Clue key={clue.id} clue={clue} />
         ))}
       </div>
     );
---
+++ src/components/Clue.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Clue extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showed: false,
+    };
+  }
+
+  handleClick() {
+    this.setState({ showed: true });
+  }
+  render() {
+    const { showed } = this.state;
+    const { answer, question, value } = this.props.clue;
+    return (
+      <div className="clue" onClick={() => this.handleClick()}>
+        <h4>{value || 'unknown'}</h4>
+        <hr />
+        <h5>{question}</h5>
+        <hr />
+        <h5
+          className={showed ? "answer text-revealed" : "answer text-hidden"}
+        >{answer}</h5>
+      </div>
+    );
+  }
+}
+
+export default Clue;
